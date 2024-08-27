8231193636f72241df8c7f4e836bc57133fc499b
Jasminqabaha <yasminqabaha@gmail.com>
2024-08-27 13:15:47 +0300

    make button working with State and updating State Based On Old State Correctl

--- Section4/main/project 2/07-tic-tac-toe-starting-project/src/components/Player.jsx
+++ Section4/main/project 2/07-tic-tac-toe-starting-project/src/components/Player.jsx
@@ -1,11 +1,27 @@
+import { useState } from "react";
 export default function Player({name,sympol}){
+    const [isEditting,setIsEditting]= useState(false);
+    function handleEditClick(){
+
+            //setIsEditting(!isEditting)
+            setIsEditting(editting=>!editting)
+       
+    }
+    let playerName= <span className="player-name">{name}</span>
+    let Button= "Edit"
+    if(isEditting){
+        playerName=<input type="text" value={name} required/>
+        Button= "Save"
+    }
+   
+
     return (
     <li>
             <span className="player">
-            <span className="player-name">{name}</span>
+            {playerName}
             <span className="player-symbol">{sympol}</span>
             </span>
-            <button>Edit</button>
+            <button onClick={()=>handleEditClick()}>{Button}</button>
     </li>
     );
 
