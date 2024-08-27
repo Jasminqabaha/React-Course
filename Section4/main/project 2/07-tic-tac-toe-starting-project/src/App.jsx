8bf5ccd7320f567cf5375b3620a990f4d3ea0865
Jasminqabaha <yasminqabaha@gmail.com>
2024-08-27 11:47:09 +0300

    I started the xo project and did the header and start the game container

--- Section4/main/project 2/07-tic-tac-toe-starting-project/src/App.jsx
+++ Section4/main/project 2/07-tic-tac-toe-starting-project/src/App.jsx
@@ -3,7 +3,21 @@ function App() {
   
 
   return (
-    <h1>React Tic-Tac-Toe</h1>
+     <main>
+      <div id="game-container">
+        <ol id="players">
+          <li className="player">
+            <span className="player-name">Name1</span>
+            <span className="player-symbol">x</span>
+          </li>
+          <li className="player">
+            <span className="player-name">Name2</span>
+            <span className="player-symbol">o</span>
+          </li>
+        </ol>
+        game poard
+      </div>
+     </main>
   )
 }
 
