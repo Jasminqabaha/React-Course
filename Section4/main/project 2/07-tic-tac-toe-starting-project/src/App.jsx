b07e981bcc1c9320d8d8a03887285519da93c561
Jasminqabaha <yasminqabaha@gmail.com>
2024-08-29 00:55:10 +0300

    4.26 Deriving State From Props on GameBoard

--- Section4/main/project 2/07-tic-tac-toe-starting-project/src/App.jsx
+++ Section4/main/project 2/07-tic-tac-toe-starting-project/src/App.jsx
@@ -1,10 +1,22 @@
 import { useState } from "react";
 import Player from "./components/Player.jsx";
 import GameBoard from "./components/GameBoard.jsx";
+import Log from "./components/Log.jsx";
 function App() {
+  const [gameTurns,setGameTurns]= useState([]);
   const [activePlayer,setActivePlayer]= useState('X');
-  function handleSellectSquare(){
+  function handleSellectSquare(rowIndex,colIndex){
     setActivePlayer(prevPlayer=> prevPlayer==='X' ? 'O' :'X' );
+    setGameTurns((prevTurns)=>{
+      let currentPlayer='X';
+      if(prevTurns.length>0 && prevTurns[0].player==='X')
+        currentPlayer='O';
+      const updatedTurnes=[
+        {square:{row:rowIndex,col:colIndex},player:currentPlayer},
+        ...prevTurns];
+        return updatedTurnes;
+    }
+    );
   }
 
   return (
@@ -14,8 +26,9 @@ function App() {
         <Player initialName="Player1" sympol='X' isActive={activePlayer==='X'}></Player>
         <Player initialName="Player2" sympol='O' isActive={activePlayer==='O'}></Player>
         </ol>
-        <GameBoard onSellectSquare={handleSellectSquare} activePlayerSympol={activePlayer}/>
+        <GameBoard onSellectSquare={handleSellectSquare} turns={gameTurns}/>
       </div>
+      <Log />
      </main>
   )
 }
--- Section4/main/project 2/07-tic-tac-toe-starting-project/src/components/GameBoard.jsx
+++ Section4/main/project 2/07-tic-tac-toe-starting-project/src/components/GameBoard.jsx
@@ -1,23 +1,30 @@
-import { useState } from "react";
 const initialGameBoarder=[
     [null,null,null],
     [null,null,null],
     [null,null,null]
 ];
 
-export default function GameBoard({onSellectSquare, activePlayerSympol}){
-    const [gameBoard,setGameBoard] = useState(initialGameBoarder);
+export default function GameBoard({onSellectSquare, turns}){
 
-    function handleSellectSquare(rowIndex,colIndex){
-        setGameBoard((prevGamePoard)=>{
-            const updatedBoard = [...prevGamePoard.map(innerArray=>[...innerArray])];
-            updatedBoard[rowIndex][colIndex]=activePlayerSympol;
-            return updatedBoard;
-        }
+    let gameBoard = initialGameBoarder;
 
-        );
-        onSellectSquare();
+    for(const turn of turns){
+        const {square, player} = turn;
+        const {row,col}= square;
+        gameBoard[row][col]=player;
     }
+    // const [gameBoard,setGameBoard] = useState(initialGameBoarder);
+
+    // function handleSellectSquare(rowIndex,colIndex){
+    //     setGameBoard((prevGamePoard)=>{
+    //         const updatedBoard = [...prevGamePoard.map(innerArray=>[...innerArray])];
+    //         updatedBoard[rowIndex][colIndex]=activePlayerSympol;
+    //         return updatedBoard;
+    //     }
+
+    //     );
+    //     onSellectSquare();
+    // }
     return (
         <ol id="game-board" >
            {gameBoard.map((row,rowIndex)=> (
@@ -25,7 +32,7 @@ export default function GameBoard({onSellectSquare, activePlayerSympol}){
                     <ol>
                         {row.map((playerSympol,colIndex)=>(
                             <li key={colIndex}>
-                                <button onClick={()=>handleSellectSquare(rowIndex,colIndex)}>{playerSympol}</button>
+                                <button onClick={()=>onSellectSquare(rowIndex,colIndex)}>{playerSympol}</button>
                             </li>
 
                         )
