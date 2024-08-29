c4fea750995eadebcbc65d85058e677d9e02eb4c
Jasminqabaha <yasminqabaha@gmail.com>
2024-08-29 20:05:03 +0300

    gameOver done

--- Section4/main/project 2/07-tic-tac-toe-starting-project/src/App.jsx
+++ Section4/main/project 2/07-tic-tac-toe-starting-project/src/App.jsx
@@ -2,15 +2,57 @@ import { useState } from "react";
 import Player from "./components/Player.jsx";
 import GameBoard from "./components/GameBoard.jsx";
 import Log from "./components/Log.jsx";
+import { WINNING_COMBINATIONS } from "./components/winning-combinations.js";
+import GameOver from "./components/gameOver.jsx";
+
+const initialGameBoarder=[
+  [null,null,null],
+  [null,null,null],
+  [null,null,null]
+];
+
+function deriveActivePlayer(gameTurns){
+  let currentPlayer='X';
+    if(gameTurns.length>0 && gameTurns[0].player==='X')
+    currentPlayer='O';
+  return currentPlayer;
+}
+
 function App() {
   const [gameTurns,setGameTurns]= useState([]);
-  const [activePlayer,setActivePlayer]= useState('X');
+  //const [activePlayer,setActivePlayer]= useState('X');
+
+   const activePlayer=deriveActivePlayer(gameTurns);
+
+  let gameBoard = initialGameBoarder;
+
+    for(const turn of gameTurns){
+        const {square, player} = turn;
+        const {row,col}= square;
+        gameBoard[row][col]=player;
+    }
+
+ let winner;
+
+  for(const combination of WINNING_COMBINATIONS){
+    const firstSquareSympol=gameBoard[combination[0].row][combination[0].column];
+    const secondSquareSympol=gameBoard[combination[1].row][combination[1].column];
+    const thirdSquareSympol=gameBoard[combination[2].row][combination[2].column];
+  
+  if(firstSquareSympol &&
+    firstSquareSympol === secondSquareSympol &&
+    firstSquareSympol === thirdSquareSympol
+  )
+  {
+    winner = firstSquareSympol;
+  }
+}
+const hasDraw = gameTurns.length===9 && !winner;
+
   function handleSellectSquare(rowIndex,colIndex){
-    setActivePlayer(prevPlayer=> prevPlayer==='X' ? 'O' :'X' );
+    // setActivePlayer(prevPlayer=> prevPlayer==='X' ? 'O' :'X' );
     setGameTurns((prevTurns)=>{
-      let currentPlayer='X';
-      if(prevTurns.length>0 && prevTurns[0].player==='X')
-        currentPlayer='O';
+      const currentPlayer=deriveActivePlayer(prevTurns);
       const updatedTurnes=[
         {square:{row:rowIndex,col:colIndex},player:currentPlayer},
         ...prevTurns];
@@ -26,7 +68,8 @@ function App() {
         <Player initialName="Player1" sympol='X' isActive={activePlayer==='X'}></Player>
         <Player initialName="Player2" sympol='O' isActive={activePlayer==='O'}></Player>
         </ol>
-        <GameBoard onSellectSquare={handleSellectSquare} turns={gameTurns}/>
+        {(winner||hasDraw) && <GameOver winner={winner} />}
+        <GameBoard onSellectSquare={handleSellectSquare} board={gameBoard} />
       </div>
       <Log turns={gameTurns}/>
      </main>
--- Section4/main/project 2/07-tic-tac-toe-starting-project/src/components/GameBoard.jsx
+++ Section4/main/project 2/07-tic-tac-toe-starting-project/src/components/GameBoard.jsx
@@ -1,38 +1,15 @@
-const initialGameBoarder=[
-    [null,null,null],
-    [null,null,null],
-    [null,null,null]
-];
 
-export default function GameBoard({onSellectSquare, turns}){
+export default function GameBoard({onSellectSquare, board}){
 
-    let gameBoard = initialGameBoarder;
-
-    for(const turn of turns){
-        const {square, player} = turn;
-        const {row,col}= square;
-        gameBoard[row][col]=player;
-    }
-    // const [gameBoard,setGameBoard] = useState(initialGameBoarder);
-
-    // function handleSellectSquare(rowIndex,colIndex){
-    //     setGameBoard((prevGamePoard)=>{
-    //         const updatedBoard = [...prevGamePoard.map(innerArray=>[...innerArray])];
-    //         updatedBoard[rowIndex][colIndex]=activePlayerSympol;
-    //         return updatedBoard;
-    //     }
-
-    //     );
-    //     onSellectSquare();
-    // }
     return (
         <ol id="game-board" >
-           {gameBoard.map((row,rowIndex)=> (
+           {board.map((row,rowIndex)=> (
                 <li key={rowIndex}>
                     <ol>
                         {row.map((playerSympol,colIndex)=>(
                             <li key={colIndex}>
-                                <button onClick={()=>onSellectSquare(rowIndex,colIndex)}>{playerSympol}</button>
+                                <button onClick={()=>onSellectSquare(rowIndex,colIndex)} disabled={playerSympol!==null}
+                                    >{playerSympol}</button>
                             </li>
 
                         )
