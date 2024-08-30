fbed42f73d04ecbb2565609e9644b81ed29c095b
Jasminqabaha <yasminqabaha@gmail.com>
2024-08-30 17:36:08 +0300

    Section5 project done!

--- Section5/main/project 1/01-starting-project-section-5/01-starting-project/src/App.jsx
+++ Section5/main/project 1/01-starting-project-section-5/01-starting-project/src/App.jsx
@@ -1,6 +1,40 @@
+import { useState } from "react";
+import Header from "./components/Header"
+import Input from "./components/Input"
+import ResultsTable from "./components/ResultsTable.jsx";
+// import {calculateInvestmentResults} from "./util/investment.js"
+
+let INITIAL_INPUT_VALUES={
+  initialInvestment:10000,
+  annualInvestment:1200,
+  expectedReturn:6,
+  duration:10,
+};
 function App() {
+const [inputValues,setInputValues]=useState(INITIAL_INPUT_VALUES);
+const valueIsValid = inputValues.duration>=1;
+
+
+function handleInputChange(inputField,newValue){
+    setInputValues(prevInputValues=>{
+        return {
+        ...prevInputValues,
+        [inputField]: +newValue
+        };
+    });
+}
+// const annualData= calculateInvestmentResults(inputValues);
+
   return (
-    <h1>React Investment Calculator</h1>
+    <>
+    <Header/>
+    <Input handleInputChange={handleInputChange} inputValues={inputValues}/>
+    {/* <ResultsTable inputValues={inputValues} /> */}
+    {valueIsValid && <ResultsTable inputValues={inputValues} />}
+
+    {!valueIsValid && <p className="center">Please enter Value greater than zero</p>}
+
+    </>
   )
 }
 
