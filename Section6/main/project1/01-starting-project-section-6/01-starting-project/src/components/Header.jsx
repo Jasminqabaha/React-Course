1b58ce3d0414dbe29ba0b944c9fe70115233445e
Jasminqabaha <yasminqabaha@gmail.com>
2024-08-31 12:03:05 +0300

    I used styled-components to apply css

--- Section6/main/project1/01-starting-project-section-6/01-starting-project/src/components/Header.jsx
+++ Section6/main/project1/01-starting-project-section-6/01-starting-project/src/components/Header.jsx
@@ -6,7 +6,7 @@ export default function Header() {
       <img src={logo} alt="A canvas" />
       <h1>ReactArt</h1>
       <p className={classes.paragraph} >A community of artists and art-lovers.</p>
-      
+
     </header>
   );
 }
