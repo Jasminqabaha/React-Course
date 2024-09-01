fb4cc02ff3de9a8c963e6683e800170b52c5e798
Jasminqabaha <yasminqabaha@gmail.com>
2024-09-01 13:16:35 +0300

    I added Section7 exercise

---
+++ Section7/exercises/exercise18/App.js
@@ -0,0 +1,19 @@
+import React from 'react';
+
+import './styles.css';
+
+// don't change the Component name "App"
+export default function App() {
+    function clickHandler(){
+        console.log('Clicked!');
+    };
+    
+    return (
+        <div>
+          <h2>You're logged in!</h2>
+          <p>Welcome to your user profile!</p>
+          <button onClick={clickHandler} >Click me!</button>
+        </div>
+    );
+}
+// 
