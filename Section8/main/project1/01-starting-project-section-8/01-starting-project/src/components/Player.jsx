903dd9b48bd82520adf0df65cdf1eb2e929aacee
Jasminqabaha <yasminqabaha@gmail.com>
2024-09-04 15:17:47 +0300

    i mannaged user input with state!

--- Section8/main/project1/01-starting-project-section-8/01-starting-project/src/components/Player.jsx
+++ Section8/main/project1/01-starting-project-section-8/01-starting-project/src/components/Player.jsx
@@ -1,10 +1,22 @@
+import { useState } from "react";
+
 export default function Player() {
+  const [name,setName]=useState('');
+  const [submitted,setSubmitted]=useState(false);
+  function handleNameChange(event){
+    setSubmitted(false);
+    setName(event.target.value);
+  }
+  function handelClickButton(){
+    setSubmitted(true);
+  }
+
   return (
     <section id="player">
-      <h2>Welcome unknown entity</h2>
+      <h2>Welcome {submitted? name : "unnoun name"}</h2>
       <p>
-        <input type="text" />
-        <button>Set Name</button>
+        <input type="text" onChange={handleNameChange} value={name}/>
+        <button onClick={handelClickButton}>Set Name</button>
       </p>
     </section>
   );
