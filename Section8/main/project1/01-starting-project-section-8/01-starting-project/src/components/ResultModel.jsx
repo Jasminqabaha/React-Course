290913d41dfa1210d3bb64c05fb74bdfb53f53c3
Jasminqabaha <yasminqabaha@gmail.com>
2024-09-06 10:35:42 +0300

    Exposed component API via the useImperativeHandle hook

--- Section8/main/project1/01-starting-project-section-8/01-starting-project/src/components/ResultModel.jsx
+++ Section8/main/project1/01-starting-project-section-8/01-starting-project/src/components/ResultModel.jsx
@@ -1,9 +1,20 @@
-import { forwardRef } from "react";
+import { forwardRef,useRef, useImperativeHandle } from "react";
 
 const ResultModel = forwardRef(function ResultModel({result,targetTime},ref){
+    const dialog = useRef();
+
+    useImperativeHandle(ref,()=>
+    {
+        return {
+            open() {
+                dialog.current.showModal();
+            }
+    };
+    }
+);
 
     return (
-        <dialog ref={ref} className="result-modal">
+        <dialog ref={dialog} className="result-modal">
             <h2>You {result}</h2>
             <p>The target time was <strong>{targetTime} seconds. </strong> </p>
             <p>You stop the timer with <strong>{targetTime} X seconds left. </strong> </p>
--- Section8/main/project1/01-starting-project-section-8/01-starting-project/src/components/TimerChallenge.jsx
+++ Section8/main/project1/01-starting-project-section-8/01-starting-project/src/components/TimerChallenge.jsx
@@ -10,7 +10,7 @@ export default function TimerChallenge({title,targetTime}){
     function handleStart(){
         timer.current=setTimeout(()=>{
             setTimeExpired(true);
-            dialog.current.showModal();
+            dialog.current.open();
         },targetTime*1000);
         setStartTimer(true);
     }
