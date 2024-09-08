eec149815e7cd97305dd63b064f96b411f54cf1c
Jasminqabaha <yasminqabaha@gmail.com>
2024-09-08 20:04:20 +0300

    managed state to switch between components

--- Section9/main/01-starting-project-section-9/01-starting-project/src/App.jsx
+++ Section9/main/01-starting-project-section-9/01-starting-project/src/App.jsx
@@ -1,11 +1,37 @@
+import { useState } from "react";
 import ProjectsSidebar from "../src/components/ProjectsSidebar.jsx"
 import NewProject from "./components/NewProject.jsx";
 import NoProjectSelected from "./components/NoProjectSelected.jsx";
+
 function App() {
+  const [projectsState,setProjectsState]=useState({
+    SelectedProjectId:undefined,
+    projects:[]
+});
+
+function handleAddProject(){
+  setProjectsState(prev=>
+  {
+    return {
+    ...prev,
+    SelectedProjectId:null
+    };
+  }
+  );
+}
+
+let content;
+if(projectsState.SelectedProjectId===undefined)
+  content = <NoProjectSelected onStartedAdding={handleAddProject}/>
+  else if(projectsState.SelectedProjectId===null)
+    content = <NewProject onStartedAdding={handleAddProject} />
+
+
+
   return (
     <main className="h-screen my-8 flex gap-8">
-      <ProjectsSidebar/>
-      <NoProjectSelected/>
+      <ProjectsSidebar onStartedAdding={handleAddProject} />
+      {content}
     </main>
   );
 }
