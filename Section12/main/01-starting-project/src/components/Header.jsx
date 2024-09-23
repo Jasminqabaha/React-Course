caa5d6438476467dac179d052450bc82fbc4b775
Jasminqabaha <yasminqabaha@gmail.com>
2024-09-23 18:21:46 +0300

    starting project

--- Section12/main/01-starting-project/src/components/Header.jsx
+++
@@ -1,8 +0,0 @@
-import logoImg from "../assets/quiz-logo.png"
-export default function Header(){
-    return( <header>
-        <img src={logoImg} alt="logo" />
-        <h1>ReactQuiz</h1>
-    </header>
-    );
-}
